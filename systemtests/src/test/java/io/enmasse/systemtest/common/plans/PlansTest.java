6a89520ddd639bb3fdb27333a6aab265409c2445
David Kornel <kornys@outlook.com>
2018-04-03 15:10:39 +0200

    Refactor code, remove unused imports etc...

--- systemtests/src/test/java/io/enmasse/systemtest/common/plans/PlansTest.java
+++ systemtests/src/test/java/io/enmasse/systemtest/common/plans/PlansTest.java
@@ -11,10 +11,9 @@ import io.enmasse.systemtest.clients.rhea.RheaClientSender;
 import io.enmasse.systemtest.resources.*;
 import io.enmasse.systemtest.standard.QueueTest;
 import io.enmasse.systemtest.standard.TopicTest;
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.jupiter.api.Tag;
 import org.slf4j.Logger;
 
